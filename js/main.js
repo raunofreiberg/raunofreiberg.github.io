a08e86908832b3f84bf07057af156baa6d37b0fd
raunofreiberg <rauno@weps.ee>
2017-07-02 12:45:49 +0300

    * Fix fonts

--- js/main.js
+++ js/main.js
@@ -7,12 +7,10 @@
         fullScreenMenu = document.querySelector(".nav__mobile-fullscreen");        
 
     function transformStart() {
-
-        strokes[0].classList.toggle("animate0")
+        strokes[0].classList.toggle("animate0");
         strokes[1].classList.toggle("hide");
         strokes[2].classList.toggle("animate2");
         fullScreenMenu.classList.toggle("show");        
-
     }
 
     icon.addEventListener("click", transformStart);
